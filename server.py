dc3a605dc00d08dbf24d958fadb912f24e5c7de6
coji <coji@techtalk.jp>
2023-08-28 23:23:18 +0900

    refactor: connect / disconnect on startup/shutdown

--- server.py
+++ server.py
@@ -2,6 +2,17 @@ from fastapi import FastAPI
 from prisma import Prisma
 
 app = FastAPI()
+prisma = Prisma()
+
+
+@app.on_event("startup")
+async def startup():
+    await prisma.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await prisma.disconnect()
 
 
 @app.get("/")
@@ -12,28 +23,17 @@ def index():
 # add a new message
 @app.post("/message")
 async def add_message(message: str):
-    prisma = Prisma()
-    await prisma.connect()
-    result = await prisma.message.create(data={"text": message})
-    await prisma.disconnect()
-    return result
+    return await prisma.message.create(data={"text": message})
 
 
 @app.get("/message")
 async def get_messages(take: int = 10, offset: int = 0):
     print(take, offset)
-    prisma = Prisma()
-    await prisma.connect()
     message_count = await prisma.message.count()
     messages = await prisma.message.find_many(take, offset, order={"createdAt": "desc"})
-    await prisma.disconnect()
     return {"message_count": message_count, "messages": messages}
 
 
 @app.get("/message/{id}")
 async def get_message(id: str):
-    prisma = Prisma()
-    await prisma.connect()
-    result = await prisma.message.find_unique(where={"id": id})
-    await prisma.disconnect()
-    return result
+    return await prisma.message.find_unique(where={"id": id})
